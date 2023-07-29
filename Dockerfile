46de4b8971ce86986f09932921286b883b79c7ee
Muthuinc <smramalingamm@gmail.com>
2023-07-29 19:53:43 +0530

    buildscript and sample webpage

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM ubuntu
 RUN apt update -y
 RUN apt install -y nginx
+COPY . /var/www/html
 EXPOSE 902
 CMD ["nginx", "-g", "daemon off;"]
---
+++ build.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+set -e
+
+# Build the image
+
+docker build -t develop:v1 .
+
+# runuing the container
+
+docker run -dp 80:80 --name develop develop:v1
+
+if curl localhost:80
+then 
+    echo "success"
+
+echo "completed"
