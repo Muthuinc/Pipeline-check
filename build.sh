854b7a7605e58cc582d2e080b32285c1acd670fd
Muthuinc <smramalingamm@gmail.com>
2023-07-29 20:38:58 +0530

    prod branch

--- build.sh
+++ build.sh
@@ -6,11 +6,11 @@ docker rm -f $(docker ps -q)
 
 # Build the image
 
-docker build -t develop:v1 .
+docker build -t prod:v1 .
 
 # runuing the container
 
-docker run -dp 80:80 --name develop develop:v1
+docker run -dp 81:80 --name prod prod:v1
 
 if curl localhost:80
 then 
